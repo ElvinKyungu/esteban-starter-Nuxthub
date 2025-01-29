121052adb40b10af012d54d9eecc8bd4955946d8
elvinkyungu <elvinkyungu.75@gmail.com>
2025-01-29 22:43:27 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['nuxt-auth-utils'],
+  runtimeConfig:{
+    oauth: {
+      github: {
+        clientId:"",//Ici le fichier .env fait le job à notre place
+        clientSecret:""//ici aussi
+      }
+    }
+  }
+})
